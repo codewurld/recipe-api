ca9f4a9acf33d6c62adbc2de01d334ff9cfbcc49
codewurld <sholaq@gmail.com>
2022-02-28 20:38:45 +0000

    added partial and whole updates of data and tested on postman -- all is working well

--- routes/recipe.js
+++ routes/recipe.js
@@ -2,6 +2,50 @@ const express = require('express');
 const router = express.Router();
 const Recipe = require('../models/Recipe')
 
+
+router.get('/', async (req, res) => {
+
+    try {
+
+        const recipes = await Recipe.find();
+
+        return res.status(200).json({
+            success: true,
+            data: recipes
+        })
+
+    } catch (err) {
+        return res.json({
+            success: false,
+            message: err
+        })
+    }
+
+})
+
+router.get('/:id', async (req, res) => {
+
+    try {
+
+        const recipe = await Recipe.findById(req.params.id);
+
+        !recipe ? res.status(404).json({
+            success: false,
+            message: "Item not found"
+        }) : res.status(200).json({
+            success: true,
+            data: recipe
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: err
+        })
+    }
+
+})
+
 router.post('/', async (req, res) => {
 
     const { dish, country, ingredients, instructions, cookingTime, createdAt } = req.body;
@@ -24,6 +68,76 @@ router.post('/', async (req, res) => {
     } catch (err) {
         return res.status(404).json({
             success: false,
+            message: err
+        })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedRecipe = await Recipe.remove({ _id: req.params.id });
+
+        return res.status(200).json({
+            success: true,
+            data: deletedRecipe,
+            message: "Item successfully deleted"
+        })
+
+    } catch (err) {
+        return res.status(404).json({
+            success: false,
+            message: "Item not found"
+        })
+    }
+
+})
+
+router.patch('/:id', async (req, res) => {
+
+    try {
+        const updatedRecipe = await Recipe.updateOne({ _id: req.params.id }, { $set: { cookingTime: req.body.cookingTime } });
+
+        !updatedRecipe ? res.status(404).json({
+            success: false,
+            message: "Item cannot be found",
+
+        }) : res.status(200).json({
+            success: true,
+            message: "Item successfully updated",
+            data: updatedRecipe
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: err
+        })
+    }
+
+})
+
+router.put('/:id', async (req, res) => {
+
+
+    try {
+        const updatedRecipe = await Recipe.findByIdAndUpdate({ _id: req.params.id }, {
+            $set: {
+                dish: req.body.dish,
+                country: req.body.country,
+                ingredients: req.body.ingredients,
+                instructions: req.body.instructions,
+                cookingTime: req.body.cookingTime,
+            }
+        });
+
+        return res.status(200).json({
+            success: true,
+            data: updatedRecipe
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            success: failure,
             message: err
         })
     }
