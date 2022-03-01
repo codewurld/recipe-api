2c97e14c7b94824abc4271d5eec1e77c1c573e2e
codewurld <sholaq@gmail.com>
2022-03-01 15:00:33 +0000

    writing basic tests for APIs

---
+++ app.test.js
@@ -0,0 +1,42 @@
+const request = require('supertest');
+const app = require('./app');
+
+// Group all tests together
+describe('Recipes API', () => {
+    it('GET /recipes --> json recipes', () => {
+        return request(app)
+            .get('/recipe')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body)
+                    .toEqual(expect.objectContaining({
+                        dish: expect.any(String),
+                        country: expect.any(String),
+                        ingredients: expect.any(String),
+                        instructions: expect.any(String),
+                        cookingTime: expect.any(String),
+                        createdAt: expect.any(Date)
+                    }))
+            })
+    })
+
+    it('GET /recipes/id --> specific recipe by ID', () => {
+
+    })
+
+    it('GET /recipes/id --> 404 if not found', () => {
+
+    })
+
+    it('POST /recipes --> created recipes', () => {
+
+    })
+
+    it('GET /recipes --> validate request body', () => {
+
+    })
+
+
+})
+
